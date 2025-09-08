697a9fedb929faae89914f3ea6dfa1b45b497667
Raynard <u1344059@umail.utah.edu>
2025-09-07 22:54:27 -0600

    Platform-Independent Graphics.cpp

--- Engine/Graphics/Graphics.cpp
+++ Engine/Graphics/Graphics.cpp
@@ -1,19 +1,17 @@
 // Includes
 //=========
 
-#include "../Graphics.h"
-
-#include "Includes.h"
-#include "../cConstantBuffer.h"
-#include "../ConstantBufferFormats.h"
-#include "../cRenderState.h"
-#include "../cShader.h"
-#include "../cVertexFormat.h"
-#include "../sContext.h"
-#include "../VertexFormats.h"
-#include "../cMesh.h"
-#include "../cEffect.h"
-#include "../cView.h"
+#include "Graphics.h"
+#include "cConstantBuffer.h"
+#include "ConstantBufferFormats.h"
+#include "cRenderState.h"
+#include "cShader.h"
+#include "cVertexFormat.h"
+#include "sContext.h"
+#include "VertexFormats.h"
+#include "cMesh.h"
+#include "cEffect.h"
+#include "cView.h"
 
 #include <Engine/Asserts/Asserts.h>
 #include <Engine/Concurrency/cEvent.h>
@@ -32,7 +30,7 @@ namespace
 	eae6320::Graphics::cView* s_view = new eae6320::Graphics::cView();
 
 	// Constant buffer object
-	eae6320::Graphics::cConstantBuffer s_constantBuffer_frame( eae6320::Graphics::ConstantBufferTypes::Frame );
+	eae6320::Graphics::cConstantBuffer s_constantBuffer_frame(eae6320::Graphics::ConstantBufferTypes::Frame);
 
 	// Submission Data
 	//----------------
@@ -86,17 +84,17 @@ namespace
 // Submission
 //-----------
 
-void eae6320::Graphics::SubmitElapsedTime( const float i_elapsedSecondCount_systemTime, const float i_elapsedSecondCount_simulationTime )
+void eae6320::Graphics::SubmitElapsedTime(const float i_elapsedSecondCount_systemTime, const float i_elapsedSecondCount_simulationTime)
 {
-	EAE6320_ASSERT( s_dataBeingSubmittedByApplicationThread );
+	EAE6320_ASSERT(s_dataBeingSubmittedByApplicationThread);
 	auto& constantData_frame = s_dataBeingSubmittedByApplicationThread->constantData_frame;
 	constantData_frame.g_elapsedSecondCount_systemTime = i_elapsedSecondCount_systemTime;
 	constantData_frame.g_elapsedSecondCount_simulationTime = i_elapsedSecondCount_simulationTime;
 }
 
-eae6320::cResult eae6320::Graphics::WaitUntilDataForANewFrameCanBeSubmitted( const unsigned int i_timeToWait_inMilliseconds )
+eae6320::cResult eae6320::Graphics::WaitUntilDataForANewFrameCanBeSubmitted(const unsigned int i_timeToWait_inMilliseconds)
 {
-	return Concurrency::WaitForEvent( s_whenDataForANewFrameCanBeSubmittedFromApplicationThread, i_timeToWait_inMilliseconds );
+	return Concurrency::WaitForEvent(s_whenDataForANewFrameCanBeSubmittedFromApplicationThread, i_timeToWait_inMilliseconds);
 }
 
 eae6320::cResult eae6320::Graphics::SignalThatAllDataForAFrameHasBeenSubmitted()
@@ -111,44 +109,41 @@ void eae6320::Graphics::RenderFrame()
 {
 	// Wait for the application loop to submit data to be rendered
 	{
-		if ( Concurrency::WaitForEvent( s_whenAllDataHasBeenSubmittedFromApplicationThread ) )
+		if (Concurrency::WaitForEvent(s_whenAllDataHasBeenSubmittedFromApplicationThread))
 		{
 			// Switch the render data pointers so that
 			// the data that the application just submitted becomes the data that will now be rendered
-			std::swap( s_dataBeingSubmittedByApplicationThread, s_dataBeingRenderedByRenderThread );
+			std::swap(s_dataBeingSubmittedByApplicationThread, s_dataBeingRenderedByRenderThread);
 			// Once the pointers have been swapped the application loop can submit new data
-			if ( !s_whenDataForANewFrameCanBeSubmittedFromApplicationThread.Signal() )
+			if (!s_whenDataForANewFrameCanBeSubmittedFromApplicationThread.Signal())
 			{
-				EAE6320_ASSERTF( false, "Couldn't signal that new graphics data can be submitted" );
-				Logging::OutputError( "Failed to signal that new render data can be submitted" );
-				UserOutput::Print( "The renderer failed to signal to the application that new graphics data can be submitted."
-					" The application is probably in a bad state and should be exited" );
+				EAE6320_ASSERTF(false, "Couldn't signal that new graphics data can be submitted");
+				Logging::OutputError("Failed to signal that new render data can be submitted");
+				UserOutput::Print("The renderer failed to signal to the application that new graphics data can be submitted."
+					" The application is probably in a bad state and should be exited");
 				return;
 			}
 		}
 		else
 		{
-			EAE6320_ASSERTF( false, "Waiting for the graphics data to be submitted failed" );
-			Logging::OutputError( "Waiting for the application loop to submit data to be rendered failed" );
-			UserOutput::Print( "The renderer failed to wait for the application to submit data to be rendered."
-				" The application is probably in a bad state and should be exited" );
+			EAE6320_ASSERTF(false, "Waiting for the graphics data to be submitted failed");
+			Logging::OutputError("Waiting for the application loop to submit data to be rendered failed");
+			UserOutput::Print("The renderer failed to wait for the application to submit data to be rendered."
+				" The application is probably in a bad state and should be exited");
 			return;
 		}
 	}
 
-	auto* const direct3dImmediateContext = sContext::g_context.direct3dImmediateContext;
-	EAE6320_ASSERT( direct3dImmediateContext );
-
 	s_view->ClearViewBuffers();
 
-	EAE6320_ASSERT( s_dataBeingRenderedByRenderThread );
+	EAE6320_ASSERT(s_dataBeingRenderedByRenderThread);
 	auto* const dataRequiredToRenderFrame = s_dataBeingRenderedByRenderThread;
 
 	// Update the frame constant buffer
 	{
 		// Copy the data from the system memory that the application owns to GPU memory
 		auto& constantData_frame = dataRequiredToRenderFrame->constantData_frame;
-		s_constantBuffer_frame.Update( &constantData_frame );
+		s_constantBuffer_frame.Update(&constantData_frame);
 	}
 
 	// Bind the shading data
@@ -174,67 +169,67 @@ void eae6320::Graphics::RenderFrame()
 // Initialize / Clean Up
 //----------------------
 
-eae6320::cResult eae6320::Graphics::Initialize( const sInitializationParameters& i_initializationParameters )
+eae6320::cResult eae6320::Graphics::Initialize(const sInitializationParameters& i_initializationParameters)
 {
 	auto result = Results::Success;
 
 	// Initialize the platform-specific context
-	if ( !( result = sContext::g_context.Initialize( i_initializationParameters ) ) )
+	if (!(result = sContext::g_context.Initialize(i_initializationParameters)))
 	{
-		EAE6320_ASSERTF( false, "Can't initialize Graphics without context" );
+		EAE6320_ASSERTF(false, "Can't initialize Graphics without context");
 		return result;
 	}
 	// Initialize the platform-independent graphics objects
 	{
-		if ( result = s_constantBuffer_frame.Initialize() )
+		if (result = s_constantBuffer_frame.Initialize())
 		{
 			// There is only a single frame constant buffer that is reused
 			// and so it can be bound at initialization time and never unbound
 			s_constantBuffer_frame.Bind(
 				// In our class both vertex and fragment shaders use per-frame constant data
-				static_cast<uint_fast8_t>( eShaderType::Vertex ) | static_cast<uint_fast8_t>( eShaderType::Fragment ) );
+				static_cast<uint_fast8_t>(eShaderType::Vertex) | static_cast<uint_fast8_t>(eShaderType::Fragment));
 		}
 		else
 		{
-			EAE6320_ASSERTF( false, "Can't initialize Graphics without frame constant buffer" );
+			EAE6320_ASSERTF(false, "Can't initialize Graphics without frame constant buffer");
 			return result;
 		}
 	}
 	// Initialize the events
 	{
-		if ( !( result = s_whenAllDataHasBeenSubmittedFromApplicationThread.Initialize( Concurrency::EventType::ResetAutomaticallyAfterBeingSignaled ) ) )
+		if (!(result = s_whenAllDataHasBeenSubmittedFromApplicationThread.Initialize(Concurrency::EventType::ResetAutomaticallyAfterBeingSignaled)))
 		{
-			EAE6320_ASSERTF( false, "Can't initialize Graphics without event for when data has been submitted from the application thread" );
+			EAE6320_ASSERTF(false, "Can't initialize Graphics without event for when data has been submitted from the application thread");
 			return result;
 		}
-		if ( !( result = s_whenDataForANewFrameCanBeSubmittedFromApplicationThread.Initialize( Concurrency::EventType::ResetAutomaticallyAfterBeingSignaled,
-			Concurrency::EventState::Signaled ) ) )
+		if (!(result = s_whenDataForANewFrameCanBeSubmittedFromApplicationThread.Initialize(Concurrency::EventType::ResetAutomaticallyAfterBeingSignaled,
+			Concurrency::EventState::Signaled)))
 		{
-			EAE6320_ASSERTF( false, "Can't initialize Graphics without event for when data can be submitted from the application thread" );
+			EAE6320_ASSERTF(false, "Can't initialize Graphics without event for when data can be submitted from the application thread");
 			return result;
 		}
 	}
 	// Initialize the views
 	{
-		if ( !( result = s_view->Initialize(i_initializationParameters) ) )
+		if (!(result = s_view->Initialize(i_initializationParameters)))
 		{
-			EAE6320_ASSERTF( false, "Can't initialize Graphics without the views" );
+			EAE6320_ASSERTF(false, "Can't initialize Graphics without the views");
 			return result;
 		}
 	}
 	// Initialize the shading data
 	{
-		if ( !( result = s_effect->Initialize() ) )
+		if (!(result = s_effect->Initialize()))
 		{
-			EAE6320_ASSERTF( false, "Can't initialize Graphics without the shading data" );
+			EAE6320_ASSERTF(false, "Can't initialize Graphics without the shading data");
 			return result;
 		}
 	}
 	// Initialize the geometry
-	{	
-		if ( !( result = s_mesh->Initialize() ) )
+	{
+		if (!(result = s_mesh->Initialize()))
 		{
-			EAE6320_ASSERTF( false, "Can't initialize Graphics without the geometry data" );
+			EAE6320_ASSERTF(false, "Can't initialize Graphics without the geometry data");
 			return result;
 		}
 	}
@@ -257,22 +252,22 @@ eae6320::cResult eae6320::Graphics::CleanUp()
 
 	{
 		const auto result_constantBuffer_frame = s_constantBuffer_frame.CleanUp();
-		if ( !result_constantBuffer_frame )
+		if (!result_constantBuffer_frame)
 		{
-			EAE6320_ASSERT( false );
-			if ( result )
+			EAE6320_ASSERT(false);
+			if (result)
 			{
 				result = result_constantBuffer_frame;
 			}
 		}
 	}
-	
+
 	{
 		const auto result_context = sContext::g_context.CleanUp();
-		if ( !result_context )
+		if (!result_context)
 		{
-			EAE6320_ASSERT( false );
-			if ( result )
+			EAE6320_ASSERT(false);
+			if (result)
 			{
 				result = result_context;
 			}
