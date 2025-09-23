aca10289685ee53e8e93cc5c245e37859235f638
Raynard <u1344059@umail.utah.edu>
2025-09-23 01:25:00 -0600

    Added gameobject class

---
+++ GameObject/cGameObject.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef EAE6320_GAMEOBJECT_CGAMEOBJECT_H
+#define EAE6320_GAMEOBJECT_CGAMEOBJECT_H
+
+namespace eae6320 {
+	namespace GameObject {
+		class cGameObject {
+		public:
+		private:
+
+		};
+	};
+}
+
+#endif // EAE6320_GAMEOBJECT_CGAMEOBJECT_H
