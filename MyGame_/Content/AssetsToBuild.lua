69e42969f1de3ee592fe41175135a1c238580717
RaynardChristian <46512489+rexay246@users.noreply.github.com>
2025-09-30 08:14:18 -0600

    fixed mesh.mesh file

--- MyGame_/Content/AssetsToBuild.lua
+++ MyGame_/Content/AssetsToBuild.lua
@@ -11,4 +11,9 @@ return
 		{ path = "Shaders/Fragment/standard.shader", arguments = { "fragment" } },
 		{ path = "Shaders/Vertex/vertexInputLayout_mesh.shader", arguments = { "vertex" } },
 	},
+
+	meshes = 
+	{
+		{ path = "Meshes/mesh.mesh" },
+	}
 }
--- Tools/MeshBuilder/cMeshBuilder.cpp
+++ Tools/MeshBuilder/cMeshBuilder.cpp
@@ -4,8 +4,6 @@
 #include <Tools/AssetBuildLibrary/Functions.h>
 
 eae6320::cResult eae6320::Assets::cMeshBuilder::Build(const std::vector<std::string>& i_arguments) {
-	strcpy((char*)m_path_target, m_path_source);
-
 	std::string errorMessage;
 	cResult result = Windows::CopyFile(m_path_source, m_path_target,
 		false, true, &errorMessage);
